f7a7c0e55c5420ead9f4bda309afbfb5d1140953
alexvo21 <54552294+alexvo21@users.noreply.github.com>
2020-11-28 09:56:12 -0700

    Update Scatterplot.java

--- Scatterplot.java
+++ Scatterplot.java
@@ -51,33 +51,33 @@ public class Scatterplot extends JFrame {
   private XYDataset createDataset() {
     XYSeriesCollection dataset = new XYSeriesCollection();
 
-    //Boys (Age,weight) series
+   //First Date Attendance series
     XYSeries series1 = new XYSeries("NOV 03");
-    series1.add(1, 72.9);
-    series1.add(2, 81.6);
-    series1.add(3, 88.9);
-    series1.add(4, 96);
-    series1.add(5, 102.1);
-    series1.add(6, 108.5);
-    series1.add(7, 113.9);
-    series1.add(8, 119.3);
-    series1.add(9, 123.8);
-    series1.add(10, 124.4);
+    series1.add(1, 20);
+    series1.add(2, 27);
+    series1.add(3, 30);
+    series1.add(4, 45);
+    series1.add(5, 57);
+    series1.add(6, 60);
+    series1.add(7, 65);
+    series1.add(8, 70);
+    series1.add(9, 75);
+    series1.add(10, 75);
 
     dataset.addSeries(series1);
     
-   //Girls (Age,weight) series
+    //Second Date Attendance series
     XYSeries series2 = new XYSeries("NOV 10");
-    series2.add(1, 72.5);
-    series2.add(2, 80.1);
-    series2.add(3, 87.2);
-    series2.add(4, 94.5);
-    series2.add(5, 101.4);
-    series2.add(6, 107.4);
-    series2.add(7, 112.8);
-    series2.add(8, 118.2);
-    series2.add(9, 122.9);
-    series2.add(10, 123.4);
+    series2.add(1, 10);
+    series2.add(2, 15);
+    series2.add(3, 23);
+    series2.add(4, 33);
+    series2.add(5, 41);
+    series2.add(6, 55);
+    series2.add(7, 59);
+    series2.add(8, 64);
+    series2.add(9, 70);
+    series2.add(10, 75);
 
     dataset.addSeries(series2);
 
