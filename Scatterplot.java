433b2031a640f8f232267b51c352b236a223112c
alexvo21 <54552294+alexvo21@users.noreply.github.com>
2020-12-02 21:29:06 -0700

    Update Scatterplot.java

--- Scatterplot.java
+++ Scatterplot.java
@@ -8,7 +8,11 @@ package finalproject;
 
 /**
  *
- * @author Alex
+ * @author Alexaner Vo
+   @author Luis Gonzalez
+   @author Nicholas Meyer
+   @author Roberto Marracino
+   @author Ryan Pan
  */
 import java.awt.Color;
 
@@ -51,7 +55,7 @@ public class Scatterplot extends JFrame {
   private XYDataset createDataset() {
     XYSeriesCollection dataset = new XYSeriesCollection();
 
-   //First Date Attendance series
+   //First Student Attendance series
     XYSeries series1 = new XYSeries("Student01");
     series1.add(1, 60);
     series1.add(2, 59);
@@ -61,16 +65,16 @@ public class Scatterplot extends JFrame {
 
     dataset.addSeries(series1);
     
-    //Second Date Attendance series
+    //Second Student Attendance series
     XYSeries series2 = new XYSeries("Student02");
     series2.add(1, 75);
     series2.add(2, 15);
     series2.add(3, 41);
     series2.add(4, 22);
-  
-
+    
     dataset.addSeries(series2);
-      //Second Date Attendance series
+    
+     //Third Student Attendance series
     XYSeries series3 = new XYSeries("Student03");
     series3.add(1, 50);
     series3.add(2, 61);
@@ -79,7 +83,7 @@ public class Scatterplot extends JFrame {
   
 
     dataset.addSeries(series3);
-    //Second Date Attendance series
+    //Fourth Student Attendance series
     XYSeries series4 = new XYSeries("Student04");
     series4.add(1, 41);
     series4.add(2, 35);
@@ -88,7 +92,7 @@ public class Scatterplot extends JFrame {
     
 
     dataset.addSeries(series4);
-    //Second Date Attendance series
+    //Fifth Student Attendance series
     XYSeries series5 = new XYSeries("Student05");
     series5.add(1, 60);
     series5.add(2, 58);
@@ -97,6 +101,7 @@ public class Scatterplot extends JFrame {
 
     dataset.addSeries(series5);
     
+    //Sixth Student Attendance Series
     XYSeries series6 = new XYSeries("Student06");
     series6.add(1, 75);
     series6.add(2, 70);
@@ -105,6 +110,7 @@ public class Scatterplot extends JFrame {
 
     dataset.addSeries(series6);
     
+    //Seventh Student Attendance Series
     XYSeries series7 = new XYSeries("Student07");
     series7.add(1, 74);
     series7.add(2, 72);
